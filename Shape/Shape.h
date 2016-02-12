10b1d20d66b2c4985c7f1e23549eb6e8bba2a3c3
christian otkjær <christian.otkjaer@gmail.com>
2016-02-12 12:11:42 +0100

    Initial Commit

---
+++ Shape/Shape.h
@@ -0,0 +1,19 @@
+//
+//  Shape.h
+//  Shape
+//
+//  Created by Christian Otkjær on 12/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shape.
+FOUNDATION_EXPORT double ShapeVersionNumber;
+
+//! Project version string for Shape.
+FOUNDATION_EXPORT const unsigned char ShapeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shape/PublicHeader.h>
+
+
